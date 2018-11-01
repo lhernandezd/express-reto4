2ccf801693696099135da1adb597f1a6e0030b29
Luis Hernández <ldhdv95@gmail.com>
2018-11-01 11:16:11 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+//Ruta
+app.get('/:nombre', (req,res) => {
+  const word = req.params.nombre[0].toUpperCase() + req.params.nombre.slice(1);
+  res.send(`<h1>Hola ${word}!</h1>`);
+});
+
+//Puerto
+app.listen(3000, () => {
+  console.log('Listening on port 3000!!');
+});
