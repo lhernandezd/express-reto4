2dd51f95535ba31f24ba109aa5c59fdb2bfc7d6b
Luis Hernández <ldhdv95@gmail.com>
2018-11-01 11:18:41 -0500

    change the route

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 
 //Ruta
-app.get('/:nombre', (req,res) => {
+app.get('/makers/:nombre', (req,res) => {
   const word = req.params.nombre[0].toUpperCase() + req.params.nombre.slice(1);
   res.send(`<h1>Hola ${word}!</h1>`);
 });
 
 //Puerto
 app.listen(3000, () => {
-  console.log('Listening on port 3000!!');
+  console.log('Listening on port 3000!');
 });
